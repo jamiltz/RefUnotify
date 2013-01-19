f6a52528ede5db2784499964131be042cdb9eb94
Fei Manheche <feimanheche@robobo.org>
2013-01-19 16:21:02 +0000

    Working Notification Broadcast via SMS and Call using Twilio API

--- sms_service/text.py
+++ sms_service/text.py
@@ -17,6 +17,7 @@ client = TwilioRestClient(account, token)
 #message = client.sms.messages.create(to=userTel, from_="+"+fromTel,
 #                                     body=messageBody)
 
+# Message is sent using RU api
 def sendMessage(tel, msg):
   #Using RS  provided SMS code
   encodedMsg = urllib.quote(msg)
@@ -24,6 +25,11 @@ def sendMessage(tel, msg):
   urllib.urlopen('http://sms.api.refunite.org/?password=179d50c6eb31188925926a5d1872e8117dc58572&number='+tel+'&message='+encodedMsg)
   print 'Message sent to user as tel:' + tel + ' and encodedMsg:' + encodedMsg
 
+def TwilioCall(tel, msg):
+  call = client.calls.create(to=tel, 
+                           from_=fromTel, 
+                           url="http://www.necto.me/notify.xml")
+  #print call.sid
 #print "Message Sent"	#debug
 # The web server.
 class MyHandler(SimpleHTTPRequestHandler):
@@ -35,9 +41,20 @@ class MyHandler(SimpleHTTPRequestHandler):
       body = form['body'].value
       print 'Got Number:', number	#debug
       print 'Got Message Body: ', body	#debug
+      TwilioCall(number, body)
       sendMessage(number, body)
       self.send_response(200)
       self.send_header('Content-type', 'text/html')
+      return
+    #Response Sent from Twilio API's
+    elif self.path == '/twiresponse':
+      form = cgi.FieldStorage(fp=self.rfile, headers=self.headers,
+      environ={'REQUEST_METHOD':'POST'})
+      data = form['data'].value
+      body = form['body'].value
+      print 'TGot data:', data	#debug
+      #print 'TGot Message Body: ', body	#debug
+      #sendMessage(number, body)
       return
     return self.do_GET()
 
