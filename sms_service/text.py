8a462c7e310e0af744466f95e4bdfc3b98004308
Fei Manheche <feimanheche@robobo.org>
2013-01-19 14:26:49 +0000

    Local Post Server for Sending Messages and Notifying users

--- sms_service/text.py
+++ sms_service/text.py
@@ -1,7 +1,11 @@
 from twilio.rest import TwilioRestClient
+import urllib
+import cgi
+from BaseHTTPServer import HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 #User Details
-userTel = "+447887495965"
+userTel = "447887495965"
 fromTel = "+442033223829"
 messageBody = "We are Ready to Rock"
 
@@ -10,7 +14,32 @@ token = os.environ['TOKEN']
 client = TwilioRestClient(account, token)
 
 #using TwilioRestClient
-message = client.sms.messages.create(to=userTel, from_=fromTel,
-                                     body=messageBody)
+#message = client.sms.messages.create(to=userTel, from_="+"+fromTel,
+#                                     body=messageBody)
 
-print "Message Sent"	#debug
+def sendMessage(tel, msg):
+  #Using RS  provided SMS code
+  encodedMsg = urllib.quote(msg)
+
+  urllib.urlopen('http://sms.api.refunite.org/?password=179d50c6eb31188925926a5d1872e8117dc58572&number='+tel+'&message='+encodedMsg)
+  print 'Message sent to user as tel:' + tel + ' and encodedMsg:' + encodedMsg
+
+#print "Message Sent"	#debug
+# The web server.
+class MyHandler(SimpleHTTPRequestHandler):
+  def do_POST(self):
+    if self.path == '/notify':
+      form = cgi.FieldStorage(fp=self.rfile, headers=self.headers,
+        environ={'REQUEST_METHOD':'POST'})
+      number = form['number'].value
+      body = form['body'].value
+      print 'Got Number:', number	#debug
+      print 'Got Message Body: ', body	#debug
+      sendMessage(number, body)
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html')
+      return
+    return self.do_GET()
+
+# Server Message local post server
+server = HTTPServer(('', 8080), MyHandler).serve_forever()
